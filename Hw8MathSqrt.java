5c4f94de5d3bbd9dc123bcd297d31a9cb526a250
Kevin Vu <97920551+KevinVu07@users.noreply.github.com>
2022-10-27 00:34:33 +1100

    Add square root homework and roman to integer homework

---
+++ Hw8MathSqrt.java
@@ -0,0 +1,37 @@
+package Algorithms;
+
+/* •Write your own Math.sqrt(int num) func
+•The Result could be the exactResult +- 0.01
+- Calculate Time Complexity  */
+
+public class Hw8MathSqrt {
+
+	public static void main(String[] args) {
+		int num = 957;
+		double squareRoot = getSquareRoot(num);
+		System.out.println("Square root of " + num + " is " + squareRoot);
+	}
+
+	public static double getSquareRoot(int num) {
+
+		double squareRoot = 0.00;
+		double quotient = 0.00;
+
+		for (int i = 100; i <= 100 * num; i++) {
+			double temp = (double) i / 100;
+			quotient = num / temp;
+
+			if (quotient == 1) {
+				squareRoot = num;
+				return squareRoot;
+			} else if (Math.abs(quotient - temp) < 0.02) {
+				squareRoot = temp;
+				return squareRoot;
+			}
+
+		}
+
+		return -1;
+	}
+
+}
